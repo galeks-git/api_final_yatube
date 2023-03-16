d10e956358347ea2680f7e12daa7b84523f08f3d
galeks <jsgroup_mail@ngs.ru>
2023-03-16 15:30:32 +0700

    sprint9 project 20230316 work

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,21 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+# from rest_framework.authtoken import views
+
+from api.views import PostViewSet, GroupViewSet, CommentViewSet
+
+
+router_v1 = routers.DefaultRouter()
+router_v1.register('posts', PostViewSet)
+router_v1.register('groups', GroupViewSet)
+router_v1.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,38 @@
 # TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from posts.models import Post, Group
+from api.serializers import PostSerializer, GroupSerializer, CommentSerializer
+from api.permissions import IsAuthorOrReadOnly
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly, IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthorOrReadOnly, IsAuthenticated]
+
+    def get_post(self):
+        return get_object_or_404(
+            Post, pk=self.kwargs.get('post_id')
+        )
+
+    def get_queryset(self):
+        return self.get_post().comments
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, post=self.get_post())
